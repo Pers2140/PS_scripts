b6345053be7cb9b55b67fdabf51655a7b608e39c
Pers2140 <Pers2140@fredonia.edu>
2022-03-13 18:32:36 -0400

    added printers and beacon-health folder

--- Custom_functions/adding_printers.ps1
+++ Custom_functions/adding_printers.ps1
@@ -1,11 +1,11 @@
-$printer_ips = '192.168.136.202','192.168.136.255','192.168.137.188','192.168.136.104','192.168.137.123','192.168.136.44'
+$printer_ips = '192.168.137.51'
 
 $count = 1
 
 foreach ($ip in $printer_ips){
    
-    $name = 'ZDesigner_printer'+$count
+    $name = 'HR-HQ-HP Color LaserJet Pro MFP M479fdn'+$count
     Add-PrinterPort -name $name  -PrinterhostAddress $ip
-    Add-Printer -name $name   -DriverName "ZDesigner ZT410-203dpi ZPL" -PortName $name
+    Add-Printer -name $name   -DriverName "HP Color LaserJet A3/11x17 PCL6 Class Driver" -PortName $name
     $global:count++
 }
