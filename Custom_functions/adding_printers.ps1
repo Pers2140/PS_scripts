3b78cf68a3f289f0c2caa89c2540fe3f0bbb4bcc
Pers2140 <Pers2140@fredonia.edu>
2022-07-29 12:45:57 -0400

    PRINTERS

--- Custom_functions/adding_printers.ps1
+++ Custom_functions/adding_printers.ps1
@@ -1,13 +1,27 @@
-$printer_ips = '192.168.137.51'
+$printer_ips = 
+10.106.31.18,     
+10.106.31.11,     
+10.106.31.17,     
+10.106.31.16,     
+10.106.31.12,     
+10.106.31.15,     
+10.106.31.21,
+10.106.31.19,
+10.106.31.14,
+10.106.31.10,
+10.106.31.20,
+10.106.31.22,
+10.106.31.25,
+10.106.31.27,
+10.106.31.2
 
 $count = 1
 
-foreach (192.168.137.181 in $printer_ips){
-   
-    $global:count++
-}
+foreach ($ip in $printer_ips){
 
-    ping 192.168.136.24
-    $name = 'Accounting Brother New'
-    Add-PrinterPort -name '192.168.137.181'  -PrinterhostAddress '192.168.137.181'
-    Add-Printer -name $name   -DriverName "Brother MFC-9330CDW Printer" -PortName '192.168.137.181'
+    $name = 'ZDesigner_printer' + $count
+    Add-PrinterPort -name $name  -PrinterhostAddress $ip
+    Add-Printer -name $name   -DriverName "ZDesigner ZT410-203dpi ZPL" -PortName $name
+    $global:count++
+
+}
